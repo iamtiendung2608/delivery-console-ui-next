bdfdb272a18292b7c0e4b48e2edd18f49aabd2c0
TonND-6572 <toiiuvn111@gmail.com>
2024-05-24 14:56:39 +0700

    map UI

--- components/Maps/map.tsx
+++ components/Maps/map.tsx
@@ -5,18 +5,17 @@ import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 
 function MyMap(props: any) {
-  const { zoom } = props
-  let position = [105.58075084879322, 20.901351928710938]
+  const { position, zoom, name, phone} = props
 
   return (
-    <MapContainer center={[10.728775, 106.7339803]} zoom={13} scrollWheelZoom={true} style={{height: 400, width: "100%"}}>
+    <MapContainer center={position} zoom={zoom} scrollWheelZoom={true} style={{height: 400, width: "100%"}}>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[10.728775, 106.7339803]}>
+        <Marker position={position}>
             <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
+                {name} <br /> Contact: {phone}.
             </Popup>
         </Marker>
     </MapContainer>
